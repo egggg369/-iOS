4c56d537142f239944fd74bf89c0dba93f5637db
egggg <1665418369@qq.com>
2020-07-13 09:03:42 +0800

    第一天

---
+++ 第一天/第一天/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  第一天
+//
+//  Created by 郭红乐 on 2020/7/13.
+//  Copyright © 2020 无. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
